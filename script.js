b2929c27aea1885a7c0c5ac6c9f698e842ae8534
Vishal Singh <vishalsingh23101999@gmail.com>
2024-05-18 20:20:09 +0530

    first changes

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(person =>{
+    if(person.profession === "developer"){
+      console.log(person);
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(person =>{
+    if(person.profession === "developer"){
+      console.log(person);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newPerson= {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(newPerson);
+  console.log(arr);
+
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr.filter(person =>{
+    if(person.profession !== "admin"){
+      console.log(person);
+    }
+  })
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 4, name: "emma", age: "22", profession: "designer" },
+    { id: 5, name: "alice", age: "23", profession: "manager" },
+    {id: 6, name: "bob", age: "25", profession: "dumbCoder"}
+  ];
+  const ConcatinateArray = arr.concat(newArr);
+  console.log(concatenateArray);
 }
